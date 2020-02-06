b6dd8cd4f8fa752b09c892c859018be044a80ad7
User1 <User1@LP-5CD9296CJL.HCLT.CORP.HCL.IN>
2020-02-06 10:42:13 +0530

    Final changes with test cases

--- src/main/java/com/healthy/foodie/exception/CustomExceptionHandler.java
+++ src/main/java/com/healthy/foodie/exception/CustomExceptionHandler.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -26,7 +27,7 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 		Map<String, Object> body = new LinkedHashMap<>();
 		body.put("timestamp", new Date());
 		body.put("status", status.value());
-		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(FieldError::getDefaultMessage)
 				.collect(Collectors.toList());
 		body.put("errors", errors);
 		return new ResponseEntity<>(body, headers, HttpStatus.OK);
